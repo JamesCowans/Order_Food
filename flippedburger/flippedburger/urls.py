d9ecf13e6a983442723459e21d863c693b84634e
James Cowans <Jamescowans@hotmail.com>
2024-03-06 22:29:07 +0000

    build basket function

--- flippedburger/flippedburger/urls.py
+++ flippedburger/flippedburger/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('basket/', include('basket.urls', namespace='basket')),
     path('', include('shop.urls', namespace='shop')),
 ]
 
